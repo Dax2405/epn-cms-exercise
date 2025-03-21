17a61e333598caab55c4b52f81ac618dd99ce5f2
dax <daxnavarrete@gmail.com>
2025-03-21 15:26:56 -0500

    feat: add Engine class to represent rocket engine with thrust and specific impulse

---
+++ src/Engine.java
@@ -0,0 +1,27 @@
+
+public class Engine {
+    private Double thrust;
+    private Double SpecificImpulse;
+
+    public Engine(Double thrust, Double specificImpulse) {
+        this.thrust = thrust;
+        SpecificImpulse = specificImpulse;
+    }
+
+    public Double getThrust() {
+        return thrust;
+    }
+
+    public void setThrust(Double thrust) {
+        this.thrust = thrust;
+    }
+
+    public Double getSpecificImpulse() {
+        return SpecificImpulse;
+    }
+
+    public void setSpecificImpulse(Double specificImpulse) {
+        SpecificImpulse = specificImpulse;
+    }
+
+}
