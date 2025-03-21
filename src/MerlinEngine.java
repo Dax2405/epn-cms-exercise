6c91b33a9bcc7a6f538ae50e74b491a1cc06287d
dax <daxnavarrete@gmail.com>
2025-03-21 15:28:21 -0500

    feat: add MerlinEngine class extending Engine with specific thrust and impulse values

---
+++ src/MerlinEngine.java
@@ -0,0 +1,9 @@
+public class MerlinEngine extends Engine {
+    public static final Double merlinThrust = 845000.0; // Newtons [N]
+    public static final Double merlinSpecificImpulse = 282.0; // Seconds [s]
+
+    public MerlinEngine() {
+        super(merlinThrust, merlinSpecificImpulse);
+    }
+
+}
