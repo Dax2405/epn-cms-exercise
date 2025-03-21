481d089494c111025a054c7812917c327f7b44c1
dax <daxnavarrete@gmail.com>
2025-03-21 15:26:29 -0500

    feat: add Astronaut class with mass property to be added to crew and to rocket

---
+++ src/Astronaut.java
@@ -0,0 +1,17 @@
+public class Astronaut {
+
+    private Double mass;
+
+    public Astronaut(Double mass) {
+        this.mass = mass;
+    }
+
+    public Double getMass() {
+        return mass;
+    }
+
+    public void setMass(Double mass) {
+        this.mass = mass;
+    }
+
+}
