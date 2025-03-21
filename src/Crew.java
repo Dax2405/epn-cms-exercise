4df8390019d4042d41e73930d1aeb2abfe2a47a5
dax <daxnavarrete@gmail.com>
2025-03-21 15:26:42 -0500

    feat: add Crew class to manage astronauts and calculate total mass

---
+++ src/Crew.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Crew {
+
+    private ArrayList<Astronaut> astronauts = new ArrayList<>();
+    private Double totalMass = 0.0;
+
+    public void addAstronaut(Astronaut astronaut) {
+        astronauts.add(astronaut);
+        totalMass += astronaut.getMass();
+    }
+
+    public Double getTotalMass() {
+        return totalMass;
+    }
+
+    public void setTotalMass(Double totalMass) {
+        this.totalMass = totalMass;
+    }
+
+}
